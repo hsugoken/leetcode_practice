9ae28cc77d2a92d4fb1dc7777daa7cce529771c4
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-17 19:51:47 -0400

    Time: 7 ms (46.28%) Space: 20.8 MB (82.04%)

--- 0543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -7,25 +7,39 @@
 """
 
       1
-  3        2
-4   5    6    7
-     8
+  3        2*
+4   5    6*    7*
+     8*
+dfs(1) -> 3+2=5
+dfs(3) -> 2+1
+dfs(4)->1
+dfs(5) -> 2
+dfs(8)-> 1
+dfs(2)-> 2
+dfs(6) -> 1
+dfs(7) -> 1
+
+
+
+
 
 """
 #calculate diameter at each node
 #calculate longest path
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.diameter = 0
+        self.diameter = 0 #3
         def dfs(node):
             if not node:
                 return 0
-            left_path = dfs(node.left)
-            right_path = dfs(node.right)
+            #node at 3
+            left_path = dfs(node.left) #1
+            right_path = dfs(node.right) #2
             self.diameter = max(self.diameter, left_path+right_path)
-            return max(left_path,right_path)+1
+            return max(left_path,right_path) + 1
         dfs(root)
         return self.diameter
 
-
+#TC: O(N)
+#SC: O(log N) / O(N)
         
