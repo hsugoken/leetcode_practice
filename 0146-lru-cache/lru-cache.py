e5073cb513a3b8a59da63e24d85d3816a9f9800c
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-18 16:33:35 -0400

    Time: 186 ms (15.13%) Space: 78.6 MB (27.33%)

--- 0146-lru-cache/lru-cache.py
+++ 0146-lru-cache/lru-cache.py
@@ -1,76 +1,65 @@
 """
-init with capacity
-get(key) => returns value of key if it exists else -1
-put(key,value) => updates the value of key if key exists else add key-value pair
-                  to the cache
-                  if #keys > capcaity => remove least recent
-get O(1) => finding => hashmap (if found remove from current and add to tail)
-put O(1) => addition + deletion => O(1) LL if we know the location
-(least recent) head -> 1 <-> 2 <-> 3 <->4 <-> tail (most recent) 
-{k1:Node(1), k2:Node(2),k3:Node(3) and k4:Node(4)}
-
+set capacity, head points to least recent tail points to most recent
+get => returns value of key if present else -1 
+put => updates value of key if present else adds key-value pair and checks for capacity and if it exceeds 
+evicts the least recent
+Algo: we will use a hashmap to map key -> Node in DLL, will give us O(1) time in get
+For put using a DLL helps as we can insert and remove using a DLL in O(1) with the get 
 """
-class ListNode:
-    def __init__(self,key=0, val=0):
+class DoublyLinkedList:
+    def __init__(self, key, val):
         self.key = key
         self.val = val
         self.next = None
         self.prev = None
-
 class LRUCache:
-
-    def __init__(self, capacity: int):
-        self.keymap = {}
-        self.capacity = capacity
-
-        self.head = ListNode(-1,-1)
-        self.tail = ListNode(-1,-1)
-
+    def __init__(self, capacity):
+        self.cap = capacity
+        self.head = DoublyLinkedList(-1,-1) #least recent
+        self.tail = DoublyLinkedList(-1,-1) #most recent
+        #connecting the head and tail nodes
         self.head.next = self.tail
         self.tail.prev = self.head
+        
+        self.nodemap = {} #maps key->node
 
-    def get(self, key: int) -> int:
-        if not key in self.keymap:
+    def get(self, key):
+        if key not in self.nodemap:
             return -1
         
-        cur_node = self.keymap[key]
+        cur_node = self.nodemap[key]
         self._remove(cur_node)
         self._add(cur_node)
-        
         return cur_node.val
 
-    def put(self, key: int, value: int) -> None:
-        if key in self.keymap:
-            # old_node = 
-            self._remove(self.keymap[key])
-        new_node = ListNode(key, value)
-        self.keymap[key] = new_node
+    def put(self, key, value):
+        if key in self.nodemap:
+            old_node = self.nodemap[key]
+            self._remove(old_node)
+        new_node = DoublyLinkedList(key, value)
+        self.nodemap[key] = new_node
         self._add(new_node)
-
-        if len(self.keymap)>self.capacity:
-            least_node = self.head.next
-            self._remove(least_node)
-            del self.keymap[least_node.key]
-        
-
+        if len(self.nodemap)>self.cap:
+            least_recent_node = self.head.next
+            self._remove(least_recent_node)
+            del self.nodemap[least_recent_node.key]
 
     def _add(self, node):
-        tail_prev = self.tail.prev
-        self.tail.prev = node
-        node.next = self.tail
-        node.prev = tail_prev
-        tail_prev.next = node
+        #we add just before the tail node
+        prev_node = self.tail.prev
+        
+        prev_node.next = node
+        node.prev = prev_node
 
-    
+        node.next = self.tail
+        self.tail.prev = node
+
+
+
     def _remove(self, node):
         prev_node = node.prev
         next_node = node.next
+        #connecting the previous and next node
         prev_node.next = next_node
         next_node.prev = prev_node
-        
-
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+    
