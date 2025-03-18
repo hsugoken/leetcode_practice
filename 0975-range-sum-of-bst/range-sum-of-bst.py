f781f3809336c9a897e336fc9d5364e66746e631
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-17 22:14:31 -0400

    Time: 5 ms (41.03%) Space: 25.1 MB (86.85%)

--- 0975-range-sum-of-bst/range-sum-of-bst.py
+++ 0975-range-sum-of-bst/range-sum-of-bst.py
@@ -4,22 +4,41 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+#         self.total = 0
+#         def dfs(node):
+#             if not node:
+#                 return 0
+#             if low<=node.val<=high:
+#                 self.total += node.val
+#                 dfs(node.left)
+#                 dfs(node.right)
+#             if node.val<low:
+#                 dfs(node.right)
+#             if node.val>high:
+#                 dfs(node.left)
+#         dfs(root)
+#         return self.total
+
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        self.total = 0
-        def dfs(node):
-            if not node:
-                return 0
-            if low<=node.val<=high:
-                self.total += node.val
-                dfs(node.left)
-                dfs(node.right)
-            if node.val<low:
-                dfs(node.right)
-            if node.val>high:
-                dfs(node.left)
-        dfs(root)
-        return self.total
+        stack = [root]
+        total = 0
+        while stack:
+            curr = stack.pop()
+            if not curr:
+                continue
+            if curr.val<low:
+                stack.append(curr.right)
+            if curr.val>high:
+                stack.append(curr.left)
+            if low<=curr.val<=high:
+                stack.append(curr.left)
+                stack.append(curr.right)
+                total += curr.val
+        return total
+
 
 #TC: O(N)
 #SC: O(N)
