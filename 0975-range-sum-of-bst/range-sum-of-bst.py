6ada913fd8f6da0d3a7208314446c345fbe8e3b9
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-17 22:17:57 -0400

    Time: 3 ms (67.74%) Space: 25.3 MB (22.32%)

--- 0975-range-sum-of-bst/range-sum-of-bst.py
+++ 0975-range-sum-of-bst/range-sum-of-bst.py
@@ -4,40 +4,53 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# class Solution:
-#     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-#         self.total = 0
-#         def dfs(node):
-#             if not node:
-#                 return 0
-#             if low<=node.val<=high:
-#                 self.total += node.val
-#                 dfs(node.left)
-#                 dfs(node.right)
-#             if node.val<low:
-#                 dfs(node.right)
-#             if node.val>high:
-#                 dfs(node.left)
-#         dfs(root)
-#         return self.total
-
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        stack = [root]
-        total = 0
-        count = 0
-        while stack:
-            cur = stack.pop()
-            if cur:
-                if low<=cur.val<=high:
-                    total += cur.val
-                    count += 1
-                if cur.val>low:
-                    stack.append(cur.left)
-                if cur.val<high:
-                    stack.append(cur.right)
-        print(f"Total:{total}, Count:{count} Avg: {total/count}")
-        return total
+        self.total = 0
+        def dfs(node):
+            if not node:
+                return 0
+            if low<=node.val<=high:
+                self.total += node.val
+                dfs(node.left)
+                dfs(node.right)
+            if node.val<low:
+                dfs(node.right)
+            if node.val>high:
+                dfs(node.left)
+        dfs(root)
+        return self.total
+
+# class Solution:
+#     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+#         if not root:
+#             return 0
+        
+#         if root.val<low:
+#             return self.rangeSumBST(root.right, low, high)
+        
+#         if root.val>high:
+#             return self.rangeSumBST(root.left, low, high)
+
+#         return root.val + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
+
+# class Solution:
+#     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+#         stack = [root]
+#         total = 0
+#         count = 0
+#         while stack:
+#             cur = stack.pop()
+#             if cur:
+#                 if low<=cur.val<=high:
+#                     total += cur.val
+#                     count += 1
+#                 if cur.val>low:
+#                     stack.append(cur.left)
+#                 if cur.val<high:
+#                     stack.append(cur.right)
+#         print(f"Total:{total}, Count:{count} Avg: {total/count}")
+#         return total
 
 #TC: O(N)
 #SC: O(N)
