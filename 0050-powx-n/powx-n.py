2d008e6ea8c40345a603954a1bc9a67db4681932
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-17 20:10:58 -0400

    Time: 0 ms (100.00%) Space: 18 MB (31.97%)

--- 0050-powx-n/powx-n.py
+++ 0050-powx-n/powx-n.py
@@ -1,3 +1,7 @@
+#-n can exceed the integer range thus to handle it n should be a 64-bit integer variable).
+"""
+Note: The standard multiplication algorithm for multiplying two numbers of d-digits each might take O(d^2) time, but most modern programming languages achieve a much lower time complexity i.e. O(d(logd)(loglogd)) by utilizing a divide-and-conquer strategy and leveraging fast fourier transforms. You can read more about the computational complexities of standard mathematical operations here
+"""
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         if n<0:
@@ -10,3 +14,5 @@ class Solution:
             x = x**2
             n=n//2
         return res
+#TC: O(log n)
+#SC: O(1)
