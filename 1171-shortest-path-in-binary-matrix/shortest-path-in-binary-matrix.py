a2232b3e4e281f526b62c5a546b85536e5217640
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-17 20:46:25 -0400

    Time: 203 ms (55.98%) Space: 19.7 MB (7.34%)

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,26 +1,30 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return -1
-        R = len(grid)
-        C = len(grid[0])
+        #we will do a BFS and maintain a visited set
         N = len(grid)
-        directions = [[1,0],[0,1],[-1,0],[0,-1],
-                      [1,1],[-1,-1],[-1,1],[1,-1]]
+        queue = deque([(0,0,1)]) #r,c,length
+        visited =set((0,0))
 
-        visit = set()
-        q = deque([(0,0,1)])
-        #def bfs(q):
-        visit.add((0,0))
-        while q:
-            r,c,dist = q.popleft()
-            if (r<0 or r>=R) or (c<0 or c>=C) or (grid[r][c]==1):
+        direct = [[1,0],[0,1],[-1,0],[0,-1],
+                  [1,1],[-1,1],[-1,-1],[1,-1]]
+        #[[0,1],
+        # [1,0]]
+        #r=0,c=0
+        #visited = {(0,0)}
+        #queue = [(0,0,1), (1,0,2), (0,1,2),(1,1,2)]
+        while queue:
+            r,c,dist = queue.popleft()
+            #checks if
+            if min(r,c)<0 or max(r,c)>=N or grid[r][c]:
                 continue
-            if r == N-1 and c == N-1:
+            if r==N-1 and c==N-1:
                 return dist
-            for dr,dc in directions:
-                nr,nc = r+dr, c+dc
-                if (nr,nc) not in visit:
-                    visit.add((nr,nc))
-                    q.append([nr,nc,dist+1])
+            
+            for dr,dc in direct:
+                if (r+dr, c+dc) not in visited:
+                    queue.append((r+dr, c+dc, dist+1))
+                    visited.add((r+dr, c+dc))
+
         return -1
+
+#TC = O(N*N) #SC = O(N*N)
