2e1ad4b723adbd478137b2dbb5324966907d92cd
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-19 17:41:56 -0400

    Time: 74 ms (27.11%) Space: 18.3 MB (41.75%)

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,13 +1,14 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        #          |
+        #0,1,2,3,4,5,6,7,8
         #1,1,1,0,0,1,0,0,0 
-        #              ^
-        #l=5
-        #r=8
-        #k=0
-        #nums[r] = 0
-        #max_len=6
+        #l = 5
+        #r = 8
+        #k = 0
+        #r-l+1 = 3
+        #nums[l] = 1
+        #nums[r=7] = 0
+        #max_len= 6
         
         max_len = float('-inf')#
         l = 0
@@ -19,8 +20,9 @@ class Solution:
                     k += 1
                 l += 1
             #k=0
-            max_len = max(max_len, r-l+1)  #4<6
+            max_len = max(max_len, r-l+1) 
         return max_len
 
-
+#TC: O(N)
+#SC: O(N)
         
