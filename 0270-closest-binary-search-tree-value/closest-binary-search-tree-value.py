2ac3c51f6adbdf117b59588c699e9aeeeb3d0feb
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-19 21:58:49 -0400

    Time: 0 ms (100.00%) Space: 19.6 MB (83.16%)

---
+++ 0270-closest-binary-search-tree-value/closest-binary-search-tree-value.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+        if not target:
+            return 0
+        cur = root
+        closest = root.val
+        while cur:
+            if target==cur.val:
+                return cur.val
+
+            if abs(closest-target)>abs(cur.val-target):
+                closest = cur.val
+            elif abs(closest-target)==abs(cur.val-target):
+                closest = min(closest, cur.val)
+            
+            if target<cur.val:
+                cur = cur.left
+            else:
+                cur = cur.right
+        return closest
