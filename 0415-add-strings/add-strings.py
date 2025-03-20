adb0f980a18bf97bc06fc490d1500e4262a3a822
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-20 19:16:03 -0400

    Time: 7 ms (59.55%) Space: 18.3 MB (40.33%)

--- 0415-add-strings/add-strings.py
+++ 0415-add-strings/add-strings.py
@@ -5,14 +5,16 @@ class Solution:
         res = []
         carry = 0
         while p1>=0 or p2>=0 or carry:
-            if p1>=0:
-                val1 = int(num1[p1])
-            else:
-                val1 = 0 
-            if p2>=0:
-                val2 = int(num2[p2])
-            else:
-                val2 = 0
+            val1 = int(num1[p1]) if p1>=0 else 0
+            val2 = int(num2[p2]) if p2>=0 else 0
+            # if p1>=0:
+            #     val1 = int(num1[p1])
+            # else:
+            #     val1 = 0 
+            # if p2>=0:
+            #     val2 = int(num2[p2])
+            # else:
+            #     val2 = 0
             total = val1+val2+carry
             carry = total//10
             res.append(str(total%10)) 
