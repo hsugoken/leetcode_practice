b45c38d6aa0c4ea35e181a68bc1e5bc01ee3a8b0
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-19 22:51:45 -0400

    Time: 2447 ms (5.00%) Space: 29.4 MB (5.49%)

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -2,15 +2,37 @@ class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         def dist(x):
             return x[0]**2+x[1]**2
-        heap = []
-        for p in points[:k]:
-            heap.append([-dist(p),p[0],p[1]])
-        heapq.heapify(heap)
-        for p in points[k:]:
-            cur_dist = -dist(p)
-            if heap[0][0]<cur_dist:
-                heapq.heappushpop(heap, [cur_dist, p[0],p[1]])
-        # print(heap)
-        return [[x[1],x[2]] for x in heap]
-#TC: O(NlogK)
-#SC: O(K)
+        nums = [[dist(point), point[0], point[1]] for point in points]
+        def quickselect(left, right):
+            # Base case
+            if left >= right:
+                return
+            pivot = nums[right][0]
+            pidx = left
+            for i in range(left, right):
+                if nums[i][0]<=pivot:
+                    nums[i], nums[pidx] = nums[pidx], nums[i]
+                    pidx += 1
+            nums[pidx], nums[right] = nums[right], nums[pidx]
+            if pidx > k-1: 
+                quickselect(left, pidx-1)
+            elif pidx < k-1: 
+                quickselect(pidx+1, right)
+            # If pidx == k-1, we're done
+        
+        # Run quickselect
+        quickselect(0, len(nums)-1) 
+        # Return the k closest points
+        return [[nums[i][1], nums[i][2]] for i in range(k)]
+        # def dist(x):
+        #     return x[0]**2+x[1]**2
+        # #compute the distances and take -ve of squared distance
+        # #add it to heap (max-heap)
+        # heap = [(-dist(point), point[0], point[1]) for point in points[:k]]
+        # heapq.heapify(heap)
+        # for point in points[k:]:
+        #     cur_dist = -dist(point)
+        #     if cur_dist>heap[0][0]:
+        #         heapq.heappushpop(heap, (cur_dist, point[0],point[1]))
+        # res = [[x[1],x[2]] for x in heap]
+        # return res
