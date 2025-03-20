78fa049ecef16062e1cb6a9cd9408444e5fe57f7
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-20 08:56:04 -0400

    Time: 0 ms (100.00%) Space: 18 MB (15.62%)

--- 1646-kth-missing-positive-number/kth-missing-positive-number.py
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.py
@@ -4,11 +4,12 @@ class Solution:
         r = len(arr)-1
         while l<=r:
             mid = l + (r-l)//2
-            missing_nos = arr[mid]-mid
-            if missing_nos>k:
-                r = mid - 1
-            else:
+            missing_nos = arr[mid]-(mid+1)
+            if missing_nos<k:
+                #
                 l = mid + 1
+            else:
+                r = mid - 1
             
         missing_current = arr[r]-r
         missing_remaining = k-missing_current
