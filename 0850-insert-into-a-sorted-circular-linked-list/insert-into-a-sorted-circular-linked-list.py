7cbdb4d0ccfb7b2646f128b8f8f8ff701479f1a9
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-19 22:08:39 -0400

    Time: 44 ms (40.08%) Space: 18.5 MB (53.22%)

--- 0850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.py
+++ 0850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.py
@@ -8,42 +8,26 @@ class Node:
 
 class Solution:
     def insert(self, head: 'Optional[Node]', insertVal: int) -> 'Node':
-        """
-          10->1-->2 <- Head
-          ^        |
-          |        v
-         .9--------5
-        target = 10
-        """
         if not head:
-            copy = ListNode(insertVal)
+            copy = Node(insertVal)
             copy.next = copy
             return copy
-        
         cur = head
-        while cur.next!=head:#cur =5
-            if cur.val<=insertVal<=cur.next.val: 
-                copy = ListNode(insertVal)
-                
+        while cur.next!=head:
+            if cur.val<=insertVal<=cur.next.val:
+                copy = Node(insertVal)
                 copy.next = cur.next
                 cur.next = copy
                 return head
-                #9>1 and 9<10 or 1>10
-            if cur.val>cur.next.val and (cur.val<insertVal or cur.next.val>insertVal):
-                copy = ListNode(insertVal)  #(10)
-                
-                copy.next = cur.next
-                cur.next = copy
-                return head
+            if cur.val>cur.next.val:
+                if cur.val<insertVal or insertVal<cur.next.val:
+                    copy = Node(insertVal)
+                    copy.next = cur.next
+                    cur.next = copy
+                    return head
             cur = cur.next
-        copy = ListNode(insertVal)
-                
+        copy = Node(insertVal)
         copy.next = cur.next
         cur.next = copy
-
         return head
-
-                
-                
-#T:O(N)
-#S:O(1)
+        
