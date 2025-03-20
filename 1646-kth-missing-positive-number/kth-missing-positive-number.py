06b18441e3d64d19040ee32aef7b0a7647dc372b
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-20 08:54:06 -0400

    Time: 0 ms (100.00%) Space: 17.9 MB (56.96%)

---
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        l = 0
+        r = len(arr)-1
+        while l<=r:
+            mid = l + (r-l)//2
+            missing_nos = arr[mid]-mid
+            if missing_nos>k:
+                r = mid - 1
+            else:
+                l = mid + 1
+            
+        missing_current = arr[r]-r
+        missing_remaining = k-missing_current
+        #k-arr[r]+r + arr[r] + 1
+        return k + l
+
+        
