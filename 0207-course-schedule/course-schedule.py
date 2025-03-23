b81b7bc8c9a96d56197e3d76c5c76f58fa3dac28
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-23 10:56:59 -0400

    Time: 5 ms (60.15%) Space: 19.2 MB (62.30%)

--- 0207-course-schedule/course-schedule.py
+++ 0207-course-schedule/course-schedule.py
@@ -1,37 +1,39 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        #It should have a pre-req that cna be taken and has no loop
-        #1)(a) Create adjacency list mapping pre-requisite to course
-        #1)(b) Calculate the indegree of the course as well
-        #2) Find out nodes with 0 indegree
-        #3) Do BFS and see if visited nodes are equal to numcourses
+        #ingeneral it is a cycle detection problem. If there is a cycle in the 
+        #graph of pre-req => course 
+        #calculate indegree of course as well
+        #Find out nodes with 0 indegree
+        #Do BFS and see if visited nodes are equal to numCourses
         
-        #1)
-        # adj = collections.defaultdict(list)
         adj = collections.defaultdict(set)
         indegree = [0]*numCourses
 
         for prereq in prerequisites:
             cur_course = prereq[0]
             req_course = prereq[1]
-            # adj[req_course].append(cur_course)
+            #req_course -> cur_course
             adj[req_course].add(cur_course)
             indegree[cur_course] += 1
-        #2)
+        #Now we find nodes with 0 indegree
+        #Basically the req-course that has no req_course
+        #A->B->C
+        #D->B
+        #Here A and D are with 0 indegree
         queue = deque()
         for i in range(numCourses):
             if indegree[i]==0:
                 queue.append(i)
-        #3)
-        visited = set()
+        #Now we do BFS
+        visit = set()
         while queue:
             node = queue.popleft()
-            visited.add(node)
+            visit.add(node)
+            #now visit all the neighbors
             for nei in adj[node]:
+                #we decrease the indegree of nei as we have already processed
+                #the current node and we moved to the nei and we have to process it
                 indegree[nei] -= 1
                 if indegree[nei]==0:
                     queue.append(nei)
-        return len(visited)==numCourses
-
-        
-        
+        return len(visit)==numCourses
