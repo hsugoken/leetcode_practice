574b70fec9cf0de7f9b78d2ad2015b37227259c5
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-23 10:29:19 -0400

    Time: 46 ms (31.48%) Space: 18.3 MB (12.10%)

--- 0893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
+++ 0893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -7,38 +7,34 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        #if k=0 then we return the target as it is at a distance 0 from itself
         if k==0:
-            return [target.val]
+             return [target.val]
         
-        #we can do BFS to convert tree to form the graph
-        #to form the graph we create the adjacency list
+        q = deque([root])
         graph = collections.defaultdict(set)
-        queue = collections.deque([root])
-        while queue:
-            node = queue.popleft()
+
+        while q:
+            node = q.popleft()
             if node.left:
-                #add the node to queue
-                queue.append(node.left)
-                #now build adjacency list
+                q.append(node.left)
                 graph[node].add(node.left)
                 graph[node.left].add(node)
             if node.right:
-                #add node to the queue
-                queue.append(node.right)
+                q.append(node.right)
                 graph[node].add(node.right)
                 graph[node.right].add(node)
-        #now we have build the graph
-        #now we will start BFS from there
-        queue = collections.deque([(target, 0)])
-        visited = set()
+        #now we will do bfs from target node
+        q = deque([(target, 0)])
         res = []
-        while queue:
-            node, dist = queue.popleft()
-            visited.add(node)
+        visit = set()
+        while q:
+            node, dist = q.popleft()
+            visit.add(node)
             if dist==k:
                 res.append(node.val)
             for nei in graph[node]:
-                if nei not in visited: 
-                    queue.append((nei, dist+1))
+                if nei not in visit:
+                    visit.add(nei)
+                    q.append((nei, dist+1))
         return res
+
