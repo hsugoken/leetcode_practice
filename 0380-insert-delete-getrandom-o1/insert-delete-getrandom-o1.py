6bdb13ae45fb0ddccf44791c8031af25b2eaddcd
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-23 14:33:20 -0400

    Time: 115 ms (79.23%) Space: 58.1 MB (11.70%)

--- 0380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,35 +1,37 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.numMap = {} #{}
-        self.numList = []#[]
-    #insert [1]
+        self.num_map = {}
+        self.num_list = []
+        
+
     def insert(self, val: int) -> bool:
-        #res = False
-        res = val not in self.numMap #True if val not in self.numMap
+        if val not in self.num_map:
+            res = True
+        else:
+            res = False
         if res:
-            self.numMap[val] = len(self.numList) #adding numMap in the last index
-            self.numList.append(val)
-        return res #False
-    #remove 1
+            self.num_map[val] = len(self.num_list)
+            self.num_list.append(val)
+        return res
+        
+
     def remove(self, val: int) -> bool:
-        res = val in self.numMap #True
+        res = val in self.num_map
         if res:
-            idx = self.numMap[val] #index to remove value from #idx = 0
-            lastVal = self.numList[-1] #1
-            #move lastVal to idx 
-            self.numList[idx] = lastVal #numList[0]=1
-            self.numMap[lastVal] = idx #numMap = {1:0}
-            #delete the val from numMap and numList
-            self.numList.pop()# 
-            del self.numMap[val]
+            idx = self.num_map[val] #get index of value
+            last_value = self.num_list[-1]
+            #move last_value to index
+            self.num_list[idx] = last_value
+            #update the hashmap
+            self.num_map[last_value] = idx
+            #now remove the last value 
+            self.num_list.pop() #remove from list
+            del self.num_map[val] #remove from hashmap
         return res
 
     def getRandom(self) -> int:
-    
-        return random.choice(self.numList)
-
-        
+        return random.choice(self.num_list)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
