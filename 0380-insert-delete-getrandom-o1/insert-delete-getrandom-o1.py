909b46f9a3b3fdcd216e245c47b1bf327aa0177d
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-23 00:35:55 -0400

    Time: 126 ms (66.00%) Space: 58.3 MB (11.70%)

--- 0380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,31 +1,34 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.numMap = {}
-        self.numList = []
-
+        self.numMap = {} #{}
+        self.numList = []#[]
+    #insert [1]
     def insert(self, val: int) -> bool:
+        #res = False
         res = val not in self.numMap #True if val not in self.numMap
         if res:
             self.numMap[val] = len(self.numList) #adding numMap in the last index
             self.numList.append(val)
-        return res
-
+        return res #False
+    #remove 1
     def remove(self, val: int) -> bool:
-        res = val in self.numMap
+        res = val in self.numMap #True
         if res:
-            idx = self.numMap[val] #index to remove value from
-            lastVal = self.numList[-1]
+            idx = self.numMap[val] #index to remove value from #idx = 0
+            lastVal = self.numList[-1] #1
             #move lastVal to idx 
-            self.numList[idx] = lastVal
-            self.numMap[lastVal] = idx
+            self.numList[idx] = lastVal #numList[0]=1
+            self.numMap[lastVal] = idx #numMap = {1:0}
             #delete the val from numMap and numList
-            self.numList.pop()
+            self.numList.pop()# 
             del self.numMap[val]
         return res
 
     def getRandom(self) -> int:
+    
         return random.choice(self.numList)
+
         
 
 
