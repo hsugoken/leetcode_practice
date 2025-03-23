dcee8c158381e257f465ed7c318661fee6280847
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-23 10:41:49 -0400

    Time: 35 ms (41.23%) Space: 23.3 MB (26.78%)

--- 0721-accounts-merge/accounts-merge.py
+++ 0721-accounts-merge/accounts-merge.py
@@ -1,57 +1,35 @@
-"""
-name = accounts[i][0]
-email = account[i][1:]
-If common email then same account
-Accounts can have same name
-return [{name}+ {email_sorted_order}]
-
-Joe: em1 - em2 
-Joe: em3 - em1
-
-Joe: jm1-jm2
-
-Pete: p1
-  
-"""
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        #get name from each account and map all emails with that name
         graph = collections.defaultdict(set)
-        email_to_name = {} #{em1:joe, em2:joe, em3:joe, jm1:joe, jm2:joe, p1:pete}
-        
-        for acc in accounts:
-            name = acc[0] #joe
-            first_email = acc[1] #em1
-            for email in acc[1:]:
-                graph[email].add(first_email) 
+        email_to_name ={}
+        for account in accounts:
+            name = account[0]
+            first_email = account[1]
+            for email in account[1:]:
+                graph[email].add(first_email)
                 graph[first_email].add(email)
 
                 email_to_name[email] = name
-        #graph
-        #{em1:{em2,em3}, em2:{em1},em3:{em1}, jm1:{jm2}, jm2:{jm1},p1:{p1}}
-        #                                      ^
-        visit = set()#{em1, em2, em3}
-        res = [] 
-
-        for email in graph:
-            if email not in visit:
-                #dfs returns sorted connected emails
-                connected_email = self.dfs(email, graph, visit) 
-                #[[joe, em1,em2, em3], [joe, jm1, jm2], [pete, p1]]
-                res.append([email_to_name[email]]+connected_email)
+        
+        visit = set()
+        res = []
+        for em in graph:
+            if em not in visit:
+                connected_email = self.dfs(em, graph, visit)
+                res.append([email_to_name[em]]+connected_email)
         return res
 
-    def dfs(self, email, graph, visit):
-        #
-        stack = [email] #em2, em3
-        visit.add(email)
-        res = []
+    def dfs(self, mail, graph, visit):
+        stack = [mail]
+        visit.add(mail)
+        result = [] 
         while stack:
             node = stack.pop()
-            res.append(node) #[em1,em2, em3]
+            result.append(node)
             for nei in graph[node]:
                 if nei not in visit:
                     visit.add(nei)
                     stack.append(nei)
-        res.sort()     
-        return res
-
+        result.sort()
+        return result
