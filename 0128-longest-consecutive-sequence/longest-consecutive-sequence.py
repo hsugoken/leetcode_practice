50265d2421c1c8cd5dd1129679c56b89ef323eae
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-03-26 16:40:05 -0400

    Time: 60 ms (33.51%) Space: 34.3 MB (53.17%)

--- 0128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,17 +1,45 @@
-class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
-        max_len = 0
-        nums_set = set(nums)
-        #Here we changed the nums to nums_set
-        #this removes any duplicate element, might be needed for this problem
-        for i in nums_set:
-            cur_num = i #nums[i]
+"""
+[10,2,3,4,11,9,5,6]
+9,10,11 
+2,3,4,5,6 
+idnetify start of longest sequence
+{10,2,3,4,11,9,5,6}
+             ^
+9 is start [10,11,] 3<5
+2 is the start [3,4,5,6]
+longest = 5  
+cur_num-1 should not be there  
 
-            if (cur_num-1) not in nums_set:
+{10,2,3,4,11,9,5,6}
+                 ^
+cur_num = 2,2+1,3+1,4+1,5+1
+cur_len = 1, 2,  3,  4,  5
+
+cur_num = 11
+cur_len = 3
+
+longest = max(cur_len, longest) = 5 #5>3
+
+"""
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #nums = set(nums)
+        longest = 0
+        nums = set(nums)
+        for n in nums:
+            if n-1 not in nums:
+                cur_num = n
                 cur_len = 1
-                while cur_num+1 in nums_set:
-                    cur_len += 1
+                #2,cur_len= 4
+                #3,4,5
+                while (cur_num + 1) in nums:
                     cur_num += 1
-                max_len = max(cur_len, max_len)
-        return max_len
+                    cur_len += 1
+                longest = max(cur_len, longest)
+        return longest
 
+        
