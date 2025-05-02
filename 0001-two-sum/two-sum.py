2e307b3c5973f35eaa8f9fd268d8766833f92ab0
hsugoken <80521644+hsugoken@users.noreply.github.com>
2025-05-02 12:58:05 -0400

    Time: 2 ms (53.25%) Space: 19 MB (24.72%)

--- 0001-two-sum/two-sum.py
+++ 0001-two-sum/two-sum.py
@@ -1,12 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # keep track of number-> index
-        #if target- current_number is in our tracked list return index, current_index
-        track = {}
-        for i, num in enumerate(nums):
-            if (target-num) in track:
-                return [track[target-num], i]
-            track[num] = i
-
-#TC: O(n)
-#SC: O(n)
+        hmap = {}
+        for i,n in enumerate(nums):
+            if (target-n) in hmap:
+                return [hmap[target-n], i]
+            hmap[n] = i
+        return -1
